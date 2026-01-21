d7f1c686cad7e0ef24a93df53d2507dbbd2d2a2c
Bug-Finderr <sudu.boi.2k5@gmail.com>
2026-01-21 21:15:36 +0530

    convex auth

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,25 @@
 import "@/global.css";
 
+import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import { ThemeProvider } from "@react-navigation/native";
 import { PortalHost } from "@rn-primitives/portal";
+import { ConvexReactClient } from "convex/react";
 import { Stack } from "expo-router";
+import * as SecureStore from "expo-secure-store";
 import { StatusBar } from "expo-status-bar";
+import { Platform } from "react-native";
 import { useUniwind } from "uniwind";
 import { NAV_THEME } from "@/lib/theme";
+
+const convex = new ConvexReactClient(process.env.EXPO_PUBLIC_CONVEX_URL!, {
+  unsavedChangesWarning: false,
+});
+
+const secureStorage = {
+  getItem: SecureStore.getItemAsync,
+  setItem: SecureStore.setItemAsync,
+  removeItem: SecureStore.deleteItemAsync,
+};
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -16,10 +30,19 @@ export default function RootLayout() {
   const { theme } = useUniwind();
 
   return (
-    <ThemeProvider value={NAV_THEME[theme ?? "light"]}>
-      <StatusBar style={theme === "dark" ? "light" : "dark"} />
-      <Stack />
-      <PortalHost />
-    </ThemeProvider>
+    <ConvexAuthProvider
+      client={convex}
+      storage={
+        Platform.OS === "android" || Platform.OS === "ios"
+          ? secureStorage
+          : undefined
+      }
+    >
+      <ThemeProvider value={NAV_THEME[theme ?? "light"]}>
+        <StatusBar style={theme === "dark" ? "light" : "dark"} />
+        <Stack />
+        <PortalHost />
+      </ThemeProvider>
+    </ConvexAuthProvider>
   );
 }
